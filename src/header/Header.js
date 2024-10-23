f18064866e6277cae6644b0d87a2d816ba3f32e8
Leandro Borges Ferreira <lehen01@gmail.com>
2024-10-23 19:25:09 +0200

    Sketch of landing page

---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Header() {
+  return (
+      <header className="header">
+        <button className="header-button">Home</button>
+        <button className="header-button">Product</button>
+        <button className="header-button">What's coming</button>
+      </header>
+  );
+}
+
+export default Header;
---
+++ src/subHeader/SubHeader.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function SubHeader() {
+  return (
+      <div className="subheader">    
+        <p className='subheader-title '>Free documentation. <span className="highlight">For the brave and true.</span> </p>        
+
+        <img src="/usage_screenshot.png" alt="Screenshot of Writeopia" />
+      </div>
+  );
+}
+
+export default SubHeader;
