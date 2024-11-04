c96271e72e6be108af81ccec24f033884e939083
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-04 12:44:38 +0100

    Fixing test

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from "next/link"
 import '../../App.css';
 
 export default function Footer() {
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Spiral as Hamburger } from 'hamburger-react'
 import LargeButton from "./large-button"
 import '../../App.css';
