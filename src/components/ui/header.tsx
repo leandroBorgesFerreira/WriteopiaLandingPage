3e62c3fec0a0f5deb8c7172b2687719148cfa961
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-19 20:54:59 +0100

    Update header.tsx

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -41,17 +41,17 @@ export default function Header() {
   return (
       <header className='flex w-screen flex-row pt-6 pb-6'>
         <Link to="/">
-          <img className='header-logo z-50 ml-6' src="/logo.png" alt="Writeopia logo" />
+          <img className='header-logo z-40 ml-6' src="/logo.png" alt="Writeopia logo" />
         </Link>
         <nav className="ml-auto pr-10 hidden md:flex items-center">
           <HeaderLink to="/">Home</HeaderLink>
           <AnimatedDropDown menuItems={secondItems} label="Product" />
           <AnimatedDropDown menuItems={thirdItems} label="Comunity" />          
         </nav>
-        <div className={`z-50 ml-auto ${isOpen ? 'visible' : 'md:hidden'}  mr-6`}>
+        <div className={`z-40 ml-auto ${isOpen ? 'visible' : 'md:hidden'}  mr-6`}>
           <Hamburger toggled={isOpen} toggle={toggleMenu} size={20} />
         </div>                      
-        <div className={`fixed inset-0 bg-gray-100 dark:bg-gray-900  z-40 pt-6 transition-all duration-300 ease-in-out ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+        <div className={`fixed inset-0 bg-gray-100 dark:bg-gray-900  z-30 pt-6 transition-all duration-300 ease-in-out ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
           <nav className="flex flex-col items-start gap-6 w-screen pt-28">
             <LargeButton>Home</LargeButton>
             <Divider />        
