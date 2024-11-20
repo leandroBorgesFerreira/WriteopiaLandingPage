6a0a71814dbcace7ac7d7cba255be9d80c5f1685
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-20 10:00:16 +0100

    spaces fix

--- src/components/ui/default-button.tsx
+++ src/components/ui/default-button.tsx
@@ -2,8 +2,8 @@ import { Link } from 'lucide-react';
 import React from 'react';
 
 const DefaultLink = ({style = {}, to = "/", children = 'Button'}) => 
-	<Link to={to} className='bg-black dark:bg-gray-700 text-white px-2.5 py-2 border border-black dark:border-gray-700 rounded-lg cursor-pointer text-lg font-bold mr-1.5 hover:bg-gray-600 hover:border-gray-600' style={style}>
-		{children}
-	</Link>;
+  <Link to={to} className='bg-black dark:bg-gray-700 text-white px-2.5 py-2 border border-black dark:border-gray-700 rounded-lg cursor-pointer text-lg font-bold mr-1.5 hover:bg-gray-600 hover:border-gray-600' style={style}>
+    {children}
+  </Link>;
 
 export default DefaultLink;
