66065020c4a69dab802137163b186a84c88bfb1b
Leandro Borges Ferreira <lehen01@gmail.com>
2024-11-20 09:58:34 +0100

    Update default-button.tsx

--- src/components/ui/default-button.tsx
+++ src/components/ui/default-button.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'lucide-react';
 import React from 'react';
 
-const DefaultLink = ({style = {}, to = "/", type = 'button', children = 'Button'}) => 
-	<Link className='bg-black dark:bg-gray-700 text-white px-2.5 py-2 border border-black dark:border-gray-700 rounded-lg cursor-pointer text-lg font-bold mr-1.5 hover:bg-gray-600 hover:border-gray-600' style={style} onClick={onClick}>
+const DefaultLink = ({style = {}, to = "/", children = 'Button'}) => 
+	<Link to={to} className='bg-black dark:bg-gray-700 text-white px-2.5 py-2 border border-black dark:border-gray-700 rounded-lg cursor-pointer text-lg font-bold mr-1.5 hover:bg-gray-600 hover:border-gray-600' style={style}>
 		{children}
 	</Link>;
 
